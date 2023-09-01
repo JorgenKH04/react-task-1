8ca171c1dc3d4c933f54b4090138927cb524e359
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-09-01 11:01:06 +0200

    Initial commit

---
+++ react-task-1/src/data/data.ts
@@ -0,0 +1,14 @@
+export const projects = [
+	{
+		name: "Streamsync",
+		info: `StreamSync is an innovative
+               streaming platform that synchronizes
+               multiple video streams in real-time.`,
+        image:
+	},
+	{},
+	{},
+	{},
+	{},
+	{},
+];
